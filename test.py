ccef4b964aacb96544a0d43116df3e48f80835c5
kmoustak <moustakas@physik.uni-bonn.de>
2018-02-18 21:08:04 +0100

    chip startup script + new configuration methods

--- test.py
+++ test.py
@@ -30,15 +30,16 @@ chip['CONF'].write()
 chip.default_conf()
 chip.disable_columns()
 chip.set_icasn_low()
-chip.set_icasn_normal()
 chip.set_ireset_high()
 chip.set_ithr_high()
-chip.mask_all()
-
+#chip.mask_all()
 chip.write_conf()
 
 chip['CONF']['DEF_CONF_N'] = 1
 chip['CONF'].write()
+
+chip.set_icasn_normal()
+chip.write_conf()
 
 #chip['CONF_SR']['SET_ICASN'].setall(False)
 #chip['CONF_SR']['SET_ICASN'][68:59] = True
