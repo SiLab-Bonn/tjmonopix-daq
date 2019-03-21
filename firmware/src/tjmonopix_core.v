9a94ea8c8b36e2e91d8c1a8cd687bc3cc700e643
Christian Bespin <bespin@physik.uni-bonn.de>
2019-03-21 17:23:19 +0100

    ENH: Core file for tjmono

--- firmware/src/tjmonopix_core.v
+++ firmware/src/tjmonopix_core.v
@@ -62,7 +62,9 @@ module tjmonopix_core (
 );
 
 // -------  MODULE ADREESSES  ------- //
-localparam GPIO_BASEADDR = 16'h0000;
+localparam VERSION = 8'h04;
+
+localparam GPIO_BASEADDR = 16'h0010;
 localparam GPIO_HIGHADDR = 16'h0100-1;
 
 localparam PULSE_INJ_BASEADDR = 16'h0100;
@@ -86,14 +88,29 @@ localparam TS_HIGHADDR = 16'h0800-1;
 localparam FIFO_BASEADDR = 16'h8000;
 localparam FIFO_HIGHADDR = 16'h9000-1;
 
-localparam TS_TDC_BASEADDR = 16'h0900;
-localparam TS_TDC_HIGHADDR = 16'h0A00-1;
+localparam TS_MON_BASEADDR = 16'h0900;
+localparam TS_MON_HIGHADDR = 16'h0A00-1;
 
 localparam TS_TLU_BASEADDR = 16'h0A00;
 localparam TS_TLU_HIGHADDR = 16'h0B00-1;
 
+localparam TS_INJ_BASEADDR = 16'h0B00;
+localparam TS_INJ_HIGHADDR = 16'h0C00-1;
+
+localparam TS_RX1_BASEADDR = 16'h0C00;
+localparam TS_RX1_HIGHADDR = 16'h0D00-1;
+
 
 // -------  USER MODULES  ------- //
+reg RD_VERSION;
+always@(posedge BUS_CLK)
+    if(BUS_ADD == 16'h0000 && BUS_RD)
+        RD_VERSION <= 1;
+    else
+        RD_VERSION <= 0;
+
+assign BUS_DATA = (RD_VERSION) ? VERSION : 8'bz;
+
 wire [15:0] GPIO_OUT;
 gpio 
 #( 
@@ -115,7 +132,7 @@ gpio
 wire RESET_CONF, RESET_BCID_CONF;
 wire EN_BX_CLK_CONF, EN_OUT_CLK_CONF, SELECTAB;
 wire READ_AB, FREEZE_AB, OUT_AB, TOK_AB;
-wire EN_HITOR_TX;
+wire EN_RST_BCID_WITH_TIMESTAMP;
 
 assign RESET_CONF = GPIO_OUT[0];
 assign RESET_BCID_CONF = GPIO_OUT[1];
@@ -123,7 +140,7 @@ assign EN_BX_CLK_CONF = GPIO_OUT[2];
 assign EN_OUT_CLK_CONF = GPIO_OUT[3];
 assign DEF_CONF = ~GPIO_OUT[4];
 assign SELECTAB = GPIO_OUT[5];
-assign EN_HITOR_TX = GPIO_OUT[5];
+assign EN_RST_BCID_WITH_TIMESTAMP = GPIO_OUT[10];
 
 wire CONF_CLK;
 assign CONF_CLK = CLK8;
@@ -158,11 +175,14 @@ assign SDO = SO_CONF;
 assign LD_CONF = SLD;
 
 wire GATE_TDC;
+wire INJECTION_MON;
     
-pulse_gen
+pulse_gen640
 #( 
     .BASEADDR(PULSE_INJ_BASEADDR), 
-    .HIGHADDR(PULSE_INJ_HIGHADDR)
+    .HIGHADDR(PULSE_INJ_HIGHADDR),
+    .CLKDV(4),
+    .OUTPUT_SIZE(2)
 ) pulse_gen_inj
 (
     .BUS_CLK(BUS_CLK),
@@ -172,9 +192,12 @@ pulse_gen
     .BUS_RD(BUS_RD),
     .BUS_WR(BUS_WR),
 
-    .PULSE_CLK(CLK40), //~CLK40),
+    .PULSE_CLK320(CLK320),
+    .PULSE_CLK160(CLK160),
+    .PULSE_CLK(CLK40),
     .EXT_START(GATE_TDC),
-    .PULSE(INJECTION)
+    .PULSE({INJECTION_MON,INJECTION}),
+    .DEBUG()
 );
 
 pulse_gen
@@ -195,35 +218,39 @@ pulse_gen
     .PULSE(GATE_TDC) 
 );    
 
-wire FE_FIFO_READ, TS_FIFO_READ, TLU_FIFO_READ, TS_TDC_FIFO_READ,TS_TLU_FIFO_READ;
-wire FE_FIFO_EMPTY, TS_FIFO_EMPTY, TLU_FIFO_EMPTY,TS_TDC_FIFO_EMPTY,TS_TLU_FIFO_EMPTY;
+wire TS_INJ_FIFO_READ,TS_MON_FIFO_READ,TS_MON_FIFO_READ_TRAILING;
+wire TS_FIFO_READ, FE_FIFO_READ, TS_TLU_FIFO_READ, TLU_FIFO_READ;
+wire TS_INJ_FIFO_EMPTY,TS_MON_FIFO_EMPTY,TS_MON_FIFO_EMPTY_TRAILING;
+wire TS_FIFO_EMPTY, FE_FIFO_EMPTY, TS_TLU_FIFO_EMPTY, TLU_FIFO_EMPTY;
 wire [31:0] FE_FIFO_DATA;
-wire [31:0] TS_FIFO_DATA;
 wire [31:0] TLU_FIFO_DATA;
-wire [31:0] TS_TDC_FIFO_DATA;
+wire [31:0] TS_MON_FIFO_DATA_TRAILING;
+wire [31:0] TS_MON_FIFO_DATA;
+wire [31:0] TS_FIFO_DATA;
+wire [31:0] TS_INJ_FIFO_DATA;
 wire [31:0] TS_TLU_FIFO_DATA;
-wire TLU_FIFO_PEEMPT_REQ;
-
 
 rrp_arbiter 
 #( 
-    .WIDTH(5)
+    .WIDTH(7)
 ) rrp_arbiter (
 
     .RST(BUS_RST),
     .CLK(BUS_CLK),
 
-    .WRITE_REQ({~TS_TDC_FIFO_EMPTY,~TS_TLU_FIFO_EMPTY, ~TS_FIFO_EMPTY, ~FE_FIFO_EMPTY, ~TLU_FIFO_EMPTY}),
-    .HOLD_REQ({4'b0, TLU_FIFO_PEEMPT_REQ}),
-    .DATA_IN({TS_TDC_FIFO_DATA,TS_TLU_FIFO_DATA,TS_FIFO_DATA,FE_FIFO_DATA, TLU_FIFO_DATA}),
-    .READ_GRANT({TS_TDC_FIFO_READ,TS_TLU_FIFO_READ,TS_FIFO_READ,FE_FIFO_READ, TLU_FIFO_READ}),
+    .WRITE_REQ({~TS_INJ_FIFO_EMPTY,~TS_MON_FIFO_EMPTY,~TS_MON_FIFO_EMPTY_TRAILING,
+                ~TS_FIFO_EMPTY, ~FE_FIFO_EMPTY, ~TS_TLU_FIFO_EMPTY, ~TLU_FIFO_EMPTY}),
+    .HOLD_REQ({7'b0}),
+    .DATA_IN({TS_INJ_FIFO_DATA,TS_MON_FIFO_DATA,TS_MON_FIFO_DATA_TRAILING,
+              TS_FIFO_DATA, FE_FIFO_DATA, TS_TLU_FIFO_DATA, TLU_FIFO_DATA}),
+    .READ_GRANT({TS_INJ_FIFO_READ,TS_MON_FIFO_READ,TS_MON_FIFO_READ_TRAILING,
+                 TS_FIFO_READ, FE_FIFO_READ, TS_TLU_FIFO_READ, TLU_FIFO_READ}),
 
     .READY_OUT(ARB_READY_OUT),
     .WRITE_OUT(ARB_WRITE_OUT),
     .DATA_OUT(ARB_DATA_OUT)
 );
 
-wire TDC_TDC_OUT, TDC_TRIG_OUT;
 wire HITOR_AB;
 
 wire [64:0] TIMESTAMP;
@@ -253,9 +280,9 @@ tlu_controller #(
     .FIFO_EMPTY(TLU_FIFO_EMPTY),
     .FIFO_DATA(TLU_FIFO_DATA),
     
-    .FIFO_PREEMPT_REQ(TLU_FIFO_PEEMPT_REQ),
+    .FIFO_PREEMPT_REQ(),
     
-    .TRIGGER({7'b0,TDC_TRIG_OUT}),
+    .TRIGGER({8'b0}),
     .TRIGGER_VETO({7'b0,FIFO_FULL}),
 	 
     .TRIGGER_ACKNOWLEDGE(TRIGGER_ACKNOWLEDGE_FLAG),
@@ -263,7 +290,7 @@ tlu_controller #(
 	 //.EXT_TRIGGER_ENABLE(TLU_EXT_TRIGGER_ENABLE)
 	 
     .TLU_TRIGGER(RJ45_TRIGGER),
-    .TLU_RESET(RJ45_RESET),
+    .TLU_RESET(1'b0),
     .TLU_BUSY(TLU_BUSY),
     .TLU_CLOCK(TLU_CLOCK),
     
@@ -271,11 +298,11 @@ tlu_controller #(
 );
 assign TRIGGER_ACKNOWLEDGE_FLAG = TRIGGER_ACCEPTED_FLAG;
 
-timestamp
+timestamp640
 #(
-    .BASEADDR(TS_BASEADDR),
-    .HIGHADDR(TS_HIGHADDR),
-    .IDENTIFIER(4'b0101)
+    .BASEADDR(TS_RX1_BASEADDR),
+    .HIGHADDR(TS_RX1_HIGHADDR),
+    .IDENTIFIER(4'b0100)
 )i_timestamp(
     .BUS_CLK(BUS_CLK),
     .BUS_ADD(BUS_ADD),
@@ -284,23 +311,24 @@ timestamp
     .BUS_WR(BUS_WR),
     .BUS_RD(BUS_RD),
     
-    .CLK(CLK40),
-	 .DI(LEMO_RX[1]),
-	 .EXT_ENABLE(GATE_TDC),
-	 .EXT_TIMESTAMP(TIMESTAMP),
+    .CLK320(CLK320),
+    .CLK160(CLK160),
+    .CLK40(CLK40),
+    .DI(LEMO_RX[1]),
+    .EXT_TIMESTAMP(TIMESTAMP),
+    .EXT_ENABLE(GATE_TDC),
 
     .FIFO_READ(TS_FIFO_READ),
     .FIFO_EMPTY(TS_FIFO_EMPTY),
     .FIFO_DATA(TS_FIFO_DATA)
-    
 );
 
-timestamp_div
+timestamp640
 #(
-    .BASEADDR(TS_TDC_BASEADDR),
-    .HIGHADDR(TS_TDC_HIGHADDR),
-    .IDENTIFIER(4'b0110)
-)i_timestamp_div_tdc(
+    .BASEADDR(TS_INJ_BASEADDR),
+    .HIGHADDR(TS_INJ_HIGHADDR),
+    .IDENTIFIER(4'b0101)
+)i_timestamp_inj(
     .BUS_CLK(BUS_CLK),
     .BUS_ADD(BUS_ADD),
     .BUS_DATA(BUS_DATA),
@@ -308,26 +336,55 @@ timestamp_div
     .BUS_WR(BUS_WR),
     .BUS_RD(BUS_RD),
     
-    .CLK40(CLK40),
-    .CLK160(CLK160),
-    .CLK320(CLK320),
-	 .DI(HITOR_B),
-	 //.DI(LEMO_RX[1]),
+     .CLK40(CLK40),
+     .CLK160(CLK160),
+     .CLK320(CLK320),
+	 .DI(INJECTION_IN),
 	 .EXT_ENABLE(GATE_TDC),
 	 .EXT_TIMESTAMP(TIMESTAMP),
 
-    .FIFO_READ(TS_TDC_FIFO_READ),
-    .FIFO_EMPTY(TS_TDC_FIFO_EMPTY),
-    .FIFO_DATA(TS_TDC_FIFO_DATA)
+    .FIFO_READ(TS_INJ_FIFO_READ),
+    .FIFO_EMPTY(TS_INJ_FIFO_EMPTY),
+    .FIFO_DATA(TS_INJ_FIFO_DATA) 
+);
+
+
+timestamp640
+#(
+    .BASEADDR(TS_MON_BASEADDR),
+    .HIGHADDR(TS_MON_HIGHADDR),
+    .IDENTIFIER(4'b0110)
+)i_timestamp_mon(
+    .BUS_CLK(BUS_CLK),
+    .BUS_ADD(BUS_ADD),
+    .BUS_DATA(BUS_DATA),
+    .BUS_RST(BUS_RST),
+    .BUS_WR(BUS_WR),
+    .BUS_RD(BUS_RD),
+    
+     .CLK40(CLK40),
+     .CLK160(CLK160),
+     .CLK320(CLK320),
+	 .DI(HITOR_B),
+	 .EXT_TIMESTAMP(TIMESTAMP),
+	 .EXT_ENABLE(GATE_TDC),
+
+    .FIFO_READ(TS_MON_FIFO_READ),
+    .FIFO_EMPTY(TS_MON_FIFO_EMPTY),
+    .FIFO_DATA(TS_MON_FIFO_DATA),
+
+    .FIFO_READ_TRAILING(TS_MON_FIFO_READ_TRAILING),
+    .FIFO_EMPTY_TRAILING(TS_MON_FIFO_EMPTY_TRAILING),
+    .FIFO_DATA_TRAILING(TS_MON_FIFO_DATA_TRAILING)
     
 );
 
-timestamp_div
+timestamp640
 #(
     .BASEADDR(TS_TLU_BASEADDR),
     .HIGHADDR(TS_TLU_HIGHADDR),
     .IDENTIFIER(4'b0111)
-)i_timestamp_div_tlu(
+)i_timestamp_tlu(
     .BUS_CLK(BUS_CLK),
     .BUS_ADD(BUS_ADD),
     .BUS_DATA(BUS_DATA),
@@ -338,9 +395,9 @@ timestamp_div
     .CLK40(CLK40),
     .CLK160(CLK160),
     .CLK320(CLK320),
-    .DI(RJ45_TRIGGER),
-	 .EXT_ENABLE(~TLU_BUSY),
-	 .EXT_TIMESTAMP(TIMESTAMP),
+    .DI(RJ45_RESET),
+	.EXT_TIMESTAMP(TIMESTAMP),
+	.EXT_ENABLE(~TLU_BUSY),
 
     .FIFO_READ(TS_TLU_FIFO_READ),
     .FIFO_EMPTY(TS_TLU_FIFO_EMPTY),
@@ -392,9 +449,12 @@ always@(negedge CLK40)
    
 reg RST_BCID_reg;
 assign RESET_BCID = RST_BCID_reg;
-always@(negedge CLK40)
-    RST_BCID_reg <= RESET_BCID_CONF;
-
+always@(negedge CLK40) begin
+    if (EN_RST_BCID_WITH_TIMESTAMP ==1 && TIMESTAMP[8:0]==9'h1F0)
+        RST_BCID_reg <= RESET_BCID_CONF;
+    else if (EN_RST_BCID_WITH_TIMESTAMP ==0)
+        RST_BCID_reg <= RESET_BCID_CONF;
+end
 
 // LED assignments
 assign LED[0] = 0;
@@ -405,7 +465,6 @@ assign LED[4] = 0;
 
 assign LEMO_TX[0] = TLU_CLOCK; // trigger clock; also connected to RJ45 output
 assign LEMO_TX[1] = TLU_BUSY;  // TLU_BUSY signal; also connected to RJ45 output. Asserted when TLU FSM has 
-assign LEMO_TX[2] = INJECTION;
+assign LEMO_TX[2] = INJECTION_MON; //injection output for monitoring, triggering external devices 
 
-//assign INJECTION_MON = INJECTION;
 endmodule
